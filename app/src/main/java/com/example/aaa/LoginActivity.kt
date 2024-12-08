a1a02096fa77959500696487f90f4a8818126b54
Danilepez <danilepez5@gmail.com>
2024-12-08 19:17:24 -0400

    pantalla_2

--- app/src/main/java/com/example/aaa/LoginActivity.kt
+++ app/src/main/java/com/example/aaa/LoginActivity.kt
@@ -2,10 +2,7 @@ package com.example.aaa
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.aaa.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
