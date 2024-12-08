1b4f5339e7471a627f034c1a089afc0225e30ac3
Danilepez <danilepez5@gmail.com>
2024-12-08 16:50:36 -0400

    pantalla_1

--- app/src/main/java/com/example/aaa/MainActivity.kt
+++ app/src/main/java/com/example/aaa/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.aaa
-
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aaa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Declarar binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inicializar binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Acción para el botón "Iniciar Sesión"
+        binding.btnLogin.setOnClickListener {
+            val intentLogin = Intent(this, MainActivity::class.java)
+            startActivity(intentLogin)
+        }
+
+        // Acción para el botón "Registrarse"
+        binding.btnRegister.setOnClickListener {
+            val intentRegister = Intent(this, MainActivity::class.java)
+            startActivity(intentRegister)
         }
     }
 }
