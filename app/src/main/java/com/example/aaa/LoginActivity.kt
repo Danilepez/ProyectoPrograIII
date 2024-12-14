68ddab826640f9c2331cecd908ea0138d05e2976
Danilepez <danilepez5@gmail.com>
2024-12-14 09:24:23 -0400

    fix pantallas

--- app/src/main/java/com/example/aaa/LoginActivity.kt
+++ app/src/main/java/com/example/aaa/LoginActivity.kt
@@ -64,7 +64,7 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Inicio de sesión: ${user?.email}", Toast.LENGTH_SHORT).show()
 
 
-                val intent = Intent(this, PantallaPrincipal::class.java)
+                val intent = Intent(this, PantallaPrincipalActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
             } else{
