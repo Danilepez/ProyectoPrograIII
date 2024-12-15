01a0d83337da5f17d422557ddb8e097f2c9b611d
Diego Gomez <diego.alejandro.gomez.alcazar@gmail.com>
2024-12-15 18:25:42 -0400

    Correccion lista

--- app/src/main/java/com/example/aaa/ContenedorActivity.kt
+++ app/src/main/java/com/example/aaa/ContenedorActivity.kt
@@ -13,10 +13,9 @@ import com.example.aaa.singletons.ProductosManager
 class ContenedorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityContenedorBinding
-    private val recyclerContenedorAdapter by lazy {RecyclerContenedorAdapter()}
-    private var showDetails: Boolean = false // Estado global de visibilidad
+    private val recyclerContenedorAdapter by lazy { RecyclerContenedorAdapter() }
+    private var showDetails: Boolean = false
 
-    // Lista que almacenará los productos recibidos
     private val productosEnContenedor = mutableListOf<Producto>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,40 +23,28 @@ class ContenedorActivity : AppCompatActivity() {
         binding = ActivityContenedorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Verificar si la lista de productos en contenedor tiene datos
         Log.d("ContenedorActivity", "Productos en contenedor: ${ProductosManager.productosEnContenedor.size}")
-
 
         binding.rvProducts.apply {
             layoutManager = LinearLayoutManager(this@ContenedorActivity)
             adapter = recyclerContenedorAdapter
         }
 
-        // Recibir los productos seleccionados desde la actividad anterior
-        // Recuperar los productos seleccionados desde el putExtra
         val productosRecibidos = intent.getSerializableExtra(ProductosManager.CLAVE_PRODUCTOS_SELECCIONADOS) as? ArrayList<Producto>
 
-        // Verificar y acumular los productos
         productosRecibidos?.forEach { productoSeleccionado ->
-            // Verificar si el producto ya existe en el contenedor
-            val productoExistente = productosEnContenedor.find { it.nombre == productoSeleccionado.nombre }
+            var productoExistente = productosEnContenedor.find { it.nombre == productoSeleccionado.nombre }
 
             if (productoExistente != null) {
-                // Si ya existe, incrementa la cantidad
                 productoExistente.cantidad += productoSeleccionado.cantidad
             } else {
-                // Si no existe, agregarlo como nuevo producto
                 productosEnContenedor.add(productoSeleccionado)
             }
         }
 
-        // Actualizar el RecyclerView con los productos acumulados
         recyclerContenedorAdapter.addDataToList(productosEnContenedor)
-        // Verificar acumulación antes de pasar al adaptador
         Log.d("ContenedorActivity", "Productos enviados al RecyclerView: ${ProductosManager.productosEnContenedor.size}")
 
-
-        // Configurar botones
         binding.btnCompactView.setOnClickListener {
             if (showDetails) {
                 showDetails = false
@@ -73,7 +60,6 @@ class ContenedorActivity : AppCompatActivity() {
         }
 
         binding.btnModify.setOnClickListener {
-            // Redirige a la pantalla de "Modificar contenedor" (Pantalla 8)
             val intent = Intent(this, ModificarContenedorActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/aaa/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/aaa/PantallaPrincipalActivity.kt
@@ -3,6 +3,8 @@ package com.example.aaa
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aaa.activities.ListasActivity
+import com.example.aaa.activities.VencenProntoActivity
 import com.example.aaa.databinding.ActivityPantallaPrincipalBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,37 +18,33 @@ class PantallaPrincipalActivity : AppCompatActivity() {
         binding = ActivityPantallaPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Configurar el clic del botón "Listas"
         binding.btnListas.setOnClickListener {
             val intent = Intent(this, ListasActivity::class.java)
             startActivity(intent)
         }
 
-        // Configurar el clic del botón "Contenedor"
         binding.btnConte.setOnClickListener {
             val intent = Intent(this, ContenedorActivity::class.java)
             startActivity(intent)
         }
 
-        // Configurar el clic del botón "Vencen Pronto"
         binding.btnVence.setOnClickListener {
             val intent = Intent(this, VencenProntoActivity::class.java)
             startActivity(intent)
         }
-        // Configura el botón de cerrar sesión
+
         binding.btnCierre.setOnClickListener {
             cerrarSesion()
         }
-    }
-    fun cerrarSesion() {
-        auth.signOut() // Cierra la sesión del usuario actual
 
-        // Redirige al usuario a la pantalla de inicio de sesión
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun cerrarSesion() {
+        auth.signOut()
         val intent = Intent(this, LoginActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
-
-        // Finaliza la actividad actual para evitar que el usuario vuelva atrás
         finish()
     }
 }
