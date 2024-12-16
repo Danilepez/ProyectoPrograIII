3a13b31b00612a539dd46f67240a199247f24fe6
Danilepez <danilepez5@gmail.com>
2024-12-16 00:33:34 -0400

    pantalla 9

--- app/src/main/java/com/example/aaa/singletons/ProductosComunes.kt
+++ app/src/main/java/com/example/aaa/singletons/ProductosComunes.kt
@@ -5,14 +5,14 @@ import com.example.aaa.dataclasses.Producto
 
 object ProductosComunes {
     val productosComunesList = listOf<Producto>(
-        Producto("Leche", "2022-12-12", "Bueno", "Lacteos", 0, R.drawable.pantalla_1),
-        Producto("Pan", "2022-12-12", "Bueno", "Panaderia", 0, R.drawable.pantalla_1),
-        Producto("Huevos", "2022-12-12", "Bueno", "Lacteos", 0, R.drawable.pantalla_1),
-        Producto("Carne", "2022-12-12", "Bueno", "Carnes", 0, R.drawable.pantalla_1),
-        Producto("Pescado", "2022-12-12", "Bueno", "Pescados", 0, R.drawable.pantalla_1),
-        Producto("Pollo", "2022-12-12", "Bueno", "Carnes", 0, R.drawable.pantalla_1),
-        Producto("Papas", "2022-12-12", "Bueno", "Verduras", 0, R.drawable.pantalla_1),
-        Producto("Tomates", "2022-12-12", "Bueno", "Verduras", 0, R.drawable.pantalla_1)
+        Producto("Leche", "", "Bueno", "Lacteos", 0, R.drawable.pantalla_1),
+        Producto("Pan", "", "Bueno", "Panaderia", 0, R.drawable.pantalla_1),
+        Producto("Huevos", "", "Bueno", "Lacteos", 0, R.drawable.pantalla_1),
+        Producto("Carne", "", "Bueno", "Carnes", 0, R.drawable.pantalla_1),
+        Producto("Pescado", "", "Bueno", "Pescados", 0, R.drawable.pantalla_1),
+        Producto("Pollo", "", "Bueno", "Carnes", 0, R.drawable.pantalla_1),
+        Producto("Papas", "", "Bueno", "Verduras", 0, R.drawable.pantalla_1),
+        Producto("Tomates", "", "Bueno", "Verduras", 0, R.drawable.pantalla_1)
     )
 }
 
