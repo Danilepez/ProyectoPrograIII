75443c10abaa5d7f8e5ec782d6a6a5223087d3ca
Danilepez <danilepez5@gmail.com>
2024-12-16 18:22:30 -0400

    pantalla login (mantener sesion activa)

--- app/src/main/java/com/example/aaa/LoginActivity.kt
+++ app/src/main/java/com/example/aaa/LoginActivity.kt
@@ -16,18 +16,15 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicializar Firebase Auth
         auth = Firebase.auth
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Verificar si el usuario ya está autenticado
         if (auth.currentUser != null) {
-            // Si el usuario está autenticado, redirigir a la pantalla principal
             val intentHomeScreen = Intent(this, PantallaPrincipalActivity::class.java)
             startActivity(intentHomeScreen)
-            finish() // Finalizar LoginActivity para evitar que el usuario regrese
+            finish()
         }
 
         // Configurar el botón de inicio de sesión
@@ -48,18 +45,15 @@ class LoginActivity : AppCompatActivity() {
             return
         }
 
-        // Intentar iniciar sesión con Firebase Auth
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { respuesta ->
             if (respuesta.isSuccessful) {
                 val user = auth.currentUser
                 Toast.makeText(this, "Bienvenido, ${user?.email}", Toast.LENGTH_SHORT).show()
 
-                // Redirigir a la pantalla principal
                 val intentHomeScreen = Intent(this, PantallaPrincipalActivity::class.java)
                 startActivity(intentHomeScreen)
-                finish() // Finalizar LoginActivity
+                finish()
             } else {
-                // Mostrar error si no se puede iniciar sesión
                 Toast.makeText(this, "Error en el inicio de sesión: ${respuesta.exception?.message}", Toast.LENGTH_SHORT).show()
             }
         }
@@ -68,13 +62,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        // Verificar si hay un usuario autenticado
         val currentUser = auth.currentUser
         if (currentUser != null) {
-            // Si ya hay un usuario autenticado, redirigir a la pantalla principal
             val intentHomeScreen = Intent(this, PantallaPrincipalActivity::class.java)
             startActivity(intentHomeScreen)
-            finish() // Finalizar LoginActivity para que el usuario no regrese
+            finish()
         }
     }
 }
